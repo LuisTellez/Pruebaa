af4e162eeccdc952322264aa2eff4736fc5c45a0
Luis Tellez <luistllz97@gmail.com>
2018-10-23 16:21:12 -0700

    Comentario

--- hola/hola/Program.cs
+++ hola/hola/Program.cs
@@ -20,5 +20,6 @@ namespace hola
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+        //Hola
     }
 }
